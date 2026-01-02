5c28e0cfbee9600c3eba69a1089ed8db8666df8c
limchaehwan <chlim20@naver.com>
2026-01-02 11:13:59 +0900

    [추가] Stomp설정, [수정] API 엔드포인트 수정

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import MemberCreate from "@/views/MemberCreate.vue";
 import LoginPage from "@/views/LoginPage.vue";
 import MemberList from "@/views/MemberList.vue";
 import SimpleWebSocket from "@/views/SimpleWebSocket.vue";
+import StompChatPage from "@/views/StompChatPage.vue";
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: "/simple/chat",
     name: "SimpleWebSocket",
     component: SimpleWebSocket,
+  },
+  {
+    path: "/chatpage",
+    name: "StompChatPage",
+    component: StompChatPage,
   },
 ];
 
---
+++ src/views/StompChatPage.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-card>
+          <v-card-title class="text-center text-h5">채팅</v-card-title>
+          <v-card-text>
+            <div class="chat-box">
+              <div v-for="(msg, index) in messages" :key="index">{{ msg }}</div>
+            </div>
+            <v-text-field
+              v-model="newMessage"
+              label="메시지 입력"
+              @keyup.enter="sendMessage"
+            />
+            <v-btn color="primary" block @click="sendMessage">전송</v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import SockJS from "sockjs-client";
+import Stomp from "webstomp-client";
+//import axios from "axios";
+
+export default {
+  data() {
+    return {
+      messages: [],
+      newMessage: "",
+      stompClient: null,
+    };
+  },
+  created() {
+    this.connectWebsocket();
+  },
+  beforeUnmount() {
+    this.disconnectWebSocket();
+  },
+  methods: {
+    connectWebsocket() {
+      // sockjs는 websocket을 내장한 향상된 js 라이브러리. http 엔드포인트 사용.
+      const sockJs = new SockJS(
+        `${import.meta.env.VITE_APP_API_BASE_URL}/connect`
+      );
+      this.stompClient = Stomp.over(sockJs);
+
+      this.stompClient.connect({}, () => {
+        this.stompClient.subscribe(`/topic/1`, (message) => {
+          this.messages.push(message.body);
+          this.scrollToBottom();
+        });
+      });
+    },
+    sendMessage() {
+      if (this.newMessage.trim() === "") return;
+      this.stompClient.send(`/publish/1`, this.newMessage);
+      this.clearInput();
+    },
+    clearInput() {
+      this.newMessage = "";
+    },
+    scrollToBottom() {
+      this.$nextTick(() => {
+        const chatBox = this.$el.querySelector(".chat-box");
+        chatBox.scrollTop = chatBox.scrollHeight;
+      });
+    },
+    disconnectWebSocket() {
+      // if (this.ws) {
+      //   this.ws.close();
+      //   console.log("연결끊김!!!");
+      //   this.ws = null;
+      // }
+    },
+  },
+};
+</script>
+
+<style>
+.chat-box {
+  height: 300px;
+  overflow-y: auto;
+  border: 1px solid #ddd;
+  margin-bottom: 10px;
+}
+</style>
